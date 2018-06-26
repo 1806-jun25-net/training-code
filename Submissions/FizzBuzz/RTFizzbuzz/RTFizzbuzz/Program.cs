b1a4f8b576eb904418b6bf59658207596b452c99
rolov3 <rolando.toledo827@gmail.com>
2018-06-26 16:25:38 -0400

    Fizzbuzz code

---
+++ Submissions/FizzBuzz/RTFizzbuzz/RTFizzbuzz/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace RTFizzbuzz
+{
+    class Program
+    {
+        static int FizzCounter, BuzzCounter, FizzbuzzCounter;
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i<1001; i++)
+            {
+                fizzbuzz(i);
+            }
+            Console.WriteLine("Fizz: " + FizzCounter);
+            Console.WriteLine("Buzz: " + BuzzCounter);
+            Console.WriteLine("Buzzfizz: " + FizzbuzzCounter);
+        }
+
+        static void fizzbuzz(int num)
+        {
+            if ((num % 3) == 0 && (num % 5) == 0)
+            {
+                Console.WriteLine("Fizzbuzz");
+                FizzbuzzCounter++;
+            }
+            else if ((num % 3) == 0)
+            {
+                Console.WriteLine("Fizz");
+                FizzCounter++;
+            }
+            else if((num % 5) == 0)
+            {
+                Console.WriteLine("Buzz");
+                BuzzCounter++;
+            }
+            else
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
