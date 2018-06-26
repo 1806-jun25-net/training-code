03977b432cc84889e4e4f05a02b1edfa6664f315
Randall Valentin <randall_valentin@yahoo.com>
2018-06-26 13:24:54 -0400

    I added a for loop.

--- Week 1/GitTestSln/GitTestSln/Program.cs
+++ Week 1/GitTestSln/GitTestSln/Program.cs
@@ -9,6 +9,12 @@ namespace GitTestSln
             Console.WriteLine("Hello World!");
             Console.WriteLine("This is my branch lets see if it syncs.");
 
+            int number = 10;
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine(i);
+            }
+
         }
     }
 }
