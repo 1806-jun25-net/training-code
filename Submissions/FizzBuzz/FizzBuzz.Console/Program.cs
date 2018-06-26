57e293e8bab78fe245229f1dde5d5910a74a9937
Wesley Schull <wcschull@bsu.edu>
2018-06-26 19:45:17 -0400

    Initial commit: cleaned up project and solution spaces.

---
+++ Submissions/FizzBuzz/FizzBuzz.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FizzBuzz.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
