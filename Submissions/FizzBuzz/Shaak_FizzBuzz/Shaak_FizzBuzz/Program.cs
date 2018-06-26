d7d3f018675ddba754923266cc137381f3534a85
Kelsey Shaak <kls26@pct.edu>
2018-06-26 16:33:30 -0400

    Added comments

--- Submissions/FizzBuzz/Shaak_FizzBuzz/Shaak_FizzBuzz/Program.cs
+++ Submissions/FizzBuzz/Shaak_FizzBuzz/Shaak_FizzBuzz/Program.cs
@@ -12,41 +12,48 @@ namespace Shaak_FizzBuzz
 
         static public void Work()
         {
+            //variables to keep track of the number of buzz, fizz, fizzbuzz, and numbers
             int buz = 0;
             int fiz = 0;
             int both = 0;
             int neither = 0;
 
+            //loop runs 1 - 1000 incrementing 1 at a time
             for (int i = 1; i < 1001; i++)
             {
+                // sees if the number can be divided by 3 and 5 evenly
                 if(i % 3 == 0 && i % 5 == 0)
                 {
-                    Console.WriteLine("FizzBuzz");
-                    both++;
+                    Console.WriteLine("FizzBuzz"); //prints fizzbuzz
+                    both++; //increments both by 1
                 }
+                //sees if the number can be divided by 3 evenly
                 else if(i % 3 == 0)
                 {
-                    Console.WriteLine("Fizz");
-                    fiz++;
+                    Console.WriteLine("Fizz"); //prints fizz
+                    fiz++; //increments fiz by 1
                 }
+                //sees if the number can be divided by 5 evenly
                 else if(i % 5 == 0)
                 {
-                    Console.WriteLine("Buzz");
-                    buz++;
+                    Console.WriteLine("Buzz"); //prints buzz
+                    buz++; //increments buz by 1
                 }
                 else
+                //if number is not divisible by 5 or 3 then print the number
                 {
-                    Console.WriteLine(i);
-                    neither++;
+                    Console.WriteLine(i); //prints number that is i
+                    neither++; //increments neither by 1
                 }
             }
-
-            Console.WriteLine("FizzBuzz: " + both + " Fizz: " + fiz + " Buzz: " + buz + " Neither: " + neither);
+            //resutls calculated and written out
+            Console.WriteLine("FizzBuzz: " + both + " Fizz: " + fiz + " Buzz: " + buz + " Neither: " + neither); 
         }
+        //Main method
         static void Main(string[] args)
         {
-            Work();
-            Console.ReadLine();
+            Work(); //runs the work method
+            Console.ReadLine(); // keeps console open to see results. Hit enter to close.
         }
     }
 }
