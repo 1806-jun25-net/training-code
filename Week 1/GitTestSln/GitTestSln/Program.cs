f2da5e8408587dc2e60ee238cdbaae39c317240a
Randall Valentin <randall_valentin@yahoo.com>
2018-06-26 13:40:35 -0400

    No change so far

--- Week 1/GitTestSln/GitTestSln/Program.cs
+++ Week 1/GitTestSln/GitTestSln/Program.cs
@@ -4,8 +4,16 @@ namespace GitTestSln
 {
     class Program
     {
+        
+
+
         static void Main(string[] args)
         {
+
+
+           
+
+
             Console.WriteLine("Hello World!");
             Console.WriteLine("This is my branch lets see if it syncs.");
 
@@ -13,8 +21,27 @@ namespace GitTestSln
             for (int i = 0; i < number; i++)
             {
                 Console.WriteLine(i);
+
+
+                if (i == 9)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                }
             }
 
+            
+
+
+
+
+
+
+
         }
+
+
+       
+
+
     }
 }
