71c99fc38f2117411e2c05cc4225ed41b46a9dcf
SHADOW <angel.luis79@yahoo.com>
2018-06-26 16:47:07 -0400

    Final Commit, Comment added to the code.

--- Submissions/FizzBuzz/AngelGuzmanFizzBuzz/AngelGuzmanFizzBuzz/Program.cs
+++ Submissions/FizzBuzz/AngelGuzmanFizzBuzz/AngelGuzmanFizzBuzz/Program.cs
@@ -9,36 +9,36 @@ namespace AngelGuzmanFizzBuzz
         {
             int Fizz = 0, Buzz = 0, Fizzbuzz = 0;
 
-            for (int i = 1; i <= 1000; i++)
+            for (int i = 1; i <= 1000; i++) // Counting from 1 to 1000.
             {
 
-                if (i % 3 == 0 && i % 5 == 0)
+                if (i % 3 == 0 && i % 5 == 0) // Cheking if it divided by the number(i) is divided by 3 and 5, and counting the Fizzbuzz.
                 {
                     Console.WriteLine(i + " ==== Fizzbuzz");
                     Fizzbuzz++;
                 }
-                else if (i % 3 == 0)
+                else if (i % 3 == 0) // Cheking if it divided by the number(i) is divided by 3, and counting the Fizz.
                 {
                     Console.WriteLine(i + " ==== Fizz");
                     Fizz++;
                 }
-                else if (i % 5 == 0)
+                else if (i % 5 == 0) // Cheking if it divided by the number(i) is divided by 5, and counting the Buzz.
                 {
                     Console.WriteLine(i + " ==== Buzz");
                     Buzz++;
                 }
                 else
                 {
-                    Console.WriteLine(i);
+                    Console.WriteLine(i); // printing the number, not divided by 3 or 5.
                 }
             }
 
-            Console.WriteLine("|Fizz: " + Fizz + "|    |Buzz: " + Buzz + "|    |Fizzbuzz: " + Fizzbuzz + "|");
+            Console.WriteLine("|Fizz: " + Fizz + "|    |Buzz: " + Buzz + "|    |Fizzbuzz: " + Fizzbuzz + "|"); // Printing how many Fizz, Buzz, Fizzbuzz 
         }
 
-        static void Menu()
+        static void Menu() // Explanation of what the program do.
         {
-            Console.WriteLine("Cheking what numbers can be divided by 3 and 5 from 1 to 1000");
+            Console.WriteLine("Cheking what numbers can be divided by 3 and 5 from 1 to 1000"); 
             Console.WriteLine("Press any key to continue....");
             Console.ReadLine();
         }
