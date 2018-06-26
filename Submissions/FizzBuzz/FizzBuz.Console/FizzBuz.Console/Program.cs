0629d710d2b0f495b467bd90477a01b5bcf85816
Daniel Ramskill <dramskil@uncc.edu>
2018-06-26 13:56:17 -0400

    Adding fizbuz solution

---
+++ Submissions/FizzBuzz/FizzBuz.Console/FizzBuz.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FizzBuz.Console
+{
+    class Program
+    {
+        static void FizBuz (int a)
+        {
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
