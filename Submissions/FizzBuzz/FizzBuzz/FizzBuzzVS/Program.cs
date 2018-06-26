a51a320d1870e67c0f0093dffa4fac81ec56cef8
Randall Valentin <randall_valentin@yahoo.com>
2018-06-26 16:05:10 -0400

    Testing the upload

---
+++ Submissions/FizzBuzz/FizzBuzz/FizzBuzzVS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FizzBuzzVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // For each number from 1 to 1000 in order,
+            // * print "Fizz" for the ones divisible by 3 (& not 5)
+            // * print "Buzz" for the ones divisible by 5 (& not 3)
+            // * print "Fizzbuzz" for the ones divisible by both 3 and 5
+            // * print the number itself, for all the rest of the numbers
+            // Also, I want to know, at the end, how many Fizz, how many Buzz, how many Fizzbuzz.
+
+            
+
+
+
+        }
+    }
+}
