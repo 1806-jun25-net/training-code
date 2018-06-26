b1f32ae51db85d3b99b23e3ad06127dd1cad3463
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-06-26 12:34:28 -0400

    say goodbye.

--- Week 1/GitTestSln/GitTestSln/Program.cs
+++ Week 1/GitTestSln/GitTestSln/Program.cs
@@ -6,7 +6,7 @@ namespace GitTestSln
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Goodbye World!");
         }
     }
 }
