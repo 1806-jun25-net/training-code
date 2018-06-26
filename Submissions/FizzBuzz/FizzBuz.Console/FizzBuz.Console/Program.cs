41449e9cdfc70a66566a510da2cf9cded23f644b
Daniel Ramskill <dramskil@uncc.edu>
2018-06-26 14:09:42 -0400

    Updating FizzBuzz

--- Submissions/FizzBuzz/FizzBuz.Console/FizzBuz.Console/Program.cs
+++ Submissions/FizzBuzz/FizzBuz.Console/FizzBuz.Console/Program.cs
@@ -6,7 +6,22 @@ namespace FizzBuz.Console
     {
         static void FizBuz (int a)
         {
-            
+            switch (a)
+            {
+                case 1:
+                    if a % 3 == 0 && a % 5 != 0
+                        Console.WriteLine("Fizz");
+                    break;
+                case 2:
+                    if a % 5 == 0 && a % 3 != 0
+                        Console.WriteLine("Buzz");
+                    break;
+                case 3:
+                    if a % 3 == 0 && a % 5 == 0
+                        Console.WriteLine("FizzBuzz")
+                default
+                    Console.WriteLine(a)
+            }
         }
         static void Main(string[] args)
         {
