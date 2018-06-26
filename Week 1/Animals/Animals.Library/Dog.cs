7c43dbab7fce9eb67a0d3ae6ec8d7d88560928cd
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-06-26 16:04:32 -0400

    add animal project for demoing classes, interfaces.

---
+++ Week 1/Animals/Animals.Library/Dog.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals.Library
+{
+    public class Dog : AAnimal
+    {
+        // field with getter and setter
+
+        //private string name;
+
+        //public string GetName()
+        //{
+        //    return name;
+        //}
+
+        //public void SetName(string newName)
+        //{
+        //    if (newName != null)
+        //    {
+        //        name = newName;
+        //    }
+        //}
+
+        public override string Name { get; set; } // auto-implemented property
+
+        // manually implemented property
+
+        private string ownerName; // backing field
+
+        public string OwnerName
+        {
+            get
+            {
+                return "Nick";
+            }
+            set
+            {
+                if (value != null)
+                {
+                    ownerName = value;
+                }
+            }
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Woof!");
+        }
+
+        public override string GoAction { get; set; } = "Walking";
+    }
+}
---
+++ Week 1/Animals/Animals.UI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Animals.Library;
+
+namespace Animals.UI
+{
+    class Program
+    {
+        static void InteractWithAnimal(IAnimal animal)
+        {
+            // get and set through property
+            animal.Name = "Fido";
+            var hisName = animal.Name;
+            Console.WriteLine(hisName);
+
+            animal.MakeSound();
+
+            // error: can't access what isn't defined on IAnimal
+            //animal.OwnerName = "Fred";
+
+            //animal = new Bird();
+
+            //Dog dog = (Dog)animal; // downcasting (explicitly)
+            //dog.OwnerName = "Fred";
+        }
+
+        static void Main(string[] args)
+        {
+            IAnimal animal = new Dog(); // upcasting (implicitly)
+
+            InteractWithAnimal(new Dog());
+            InteractWithAnimal(new Bird());
+        }
+    }
+}
