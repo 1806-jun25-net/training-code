b45a19729114acba2392d772b770266df239a569
Wesley Schull <wcschull@bsu.edu>
2018-06-26 20:00:13 -0400

    Implemented FizzBuzz().

--- Submissions/FizzBuzz/FizzBuzz.Console/Program.cs
+++ Submissions/FizzBuzz/FizzBuzz.Console/Program.cs
@@ -11,7 +11,33 @@ namespace FizzBuzz.Console
 
         static void FizzBuzz()
         {
+            int fizzes = 0, buzzes = 0, fizzbuzzes = 0;
+            
+            for(int i = 1; i <= 1000; i++)
+            {
+                if(i % 3 == 0 & i % 5 == 0)
+                {
+                    System.Console.WriteLine("Fizzbuzz");
+                    fizzbuzzes++;
+                }
+                else if(i % 3 == 0)
+                {
+                    System.Console.WriteLine("Fizz");
+                    fizzes++;
+                }
+                else if(i % 5 == 0)
+                {
+                    System.Console.WriteLine("Buzz");
+                    buzzes++;
+                }
+                else
+                {
+                    System.Console.WriteLine(i);
+                }
+            }
 
+            System.Console.WriteLine($"There were {fizzes} Fizzes, {buzzes} Buzzes, " +
+                $"and {fizzbuzzes} Fizzbuzzes.");
         }
     }
 }
