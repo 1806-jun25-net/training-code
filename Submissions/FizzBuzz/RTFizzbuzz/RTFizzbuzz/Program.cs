e5e7c21a01e140cb04a7cfdc9c0c181d21823957
rolov3 <rolando.toledo827@gmail.com>
2018-06-27 15:26:07 -0400

    Minor changes in the syntax

--- Submissions/FizzBuzz/RTFizzbuzz/RTFizzbuzz/Program.cs
+++ Submissions/FizzBuzz/RTFizzbuzz/RTFizzbuzz/Program.cs
@@ -19,17 +19,17 @@ namespace RTFizzbuzz
 
         static void fizzbuzz(int num)
         {
-            if ((num % 3) == 0 && (num % 5) == 0)
+            if (num % 3 == 0 && num % 5 == 0)
             {
                 Console.WriteLine("Fizzbuzz");
                 FizzbuzzCounter++;
             }
-            else if ((num % 3) == 0)
+            else if (num % 3 == 0)
             {
                 Console.WriteLine("Fizz");
                 FizzCounter++;
             }
-            else if((num % 5) == 0)
+            else if(num % 5 == 0)
             {
                 Console.WriteLine("Buzz");
                 BuzzCounter++;
