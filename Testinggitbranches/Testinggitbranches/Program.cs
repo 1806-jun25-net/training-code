7562693a23ddcce7e5c81cb86b9a83193d4d1ddb
Phil <philipaplatt@gmail.com>
2018-06-28 11:59:18 -0400

    yep

---
+++ Testinggitbranches/Testinggitbranches/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Testinggitbranches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
