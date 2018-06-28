4b4d9b47343fd2ab6a1ef21277841dd7cd15c7b1
Chris Coronado <cbcoronado@live.com>
2018-06-28 16:57:03 -0400

    Initial commit. Training 6/28/18

---
+++ Week 1/coronado/Singleton/Singleton.Library/TimeProvider.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton.Library
+{
+    // Singleton class pattern
+    public class TimeProvider
+    {
+        // a property with just a getter, no setter
+        public virtual DateTime CurrentTime => DateTime.Now;
+
+        private static TimeProvider s_instance;
+        private static TimeProvider s_current;
+
+        public static void ResetToDefaultInstance()
+        {
+
+        }
+
+        public static TimeProvider Current
+        {
+            get
+            {
+                // s_instance is only instantiated once
+                if (s_instance != null)
+                {
+                    s_instance = new TimeProvider();
+                }
+                if (s_current == null)
+                {
+                    s_current = s_instance;
+                }
+                return s_current;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    s_instance = value;
+                }
+            }
+        }
+
+
+        protected TimeProvider() { }
+    }
+}
