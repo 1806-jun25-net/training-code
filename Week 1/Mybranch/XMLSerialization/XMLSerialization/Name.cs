a86378b0deadf230661e444b84cca132d8ee7e7f
WALTER RAMIREZ PADILLA <wram1994@gmail.com>
2018-06-28 14:02:21 -0400

    Finally

---
+++ Week 1/Mybranch/XMLSerialization/XMLSerialization/Name.cs
@@ -0,0 +1,14 @@
+﻿namespace XMLSerialization
+{
+    public class Name
+
+
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+        
+
+
+
+    }
+}
---
+++ Week 1/Mybranch/XMLSerialization/XMLSerialization/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace XMLSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Person>();
+            Fillist(list);
+            SerializetoFile("data.xml", list);
+
+            
+        }
+
+        private static void SerializetoFile(string fileName, List<Person> people)
+        {
+
+            var serializer = new XmlSerializer(typeof(List<Person>));
+            var filestream = new FileStream(fileName, FileMode.Create);
+            serializer.Serialize(filestream, people);
+
+
+
+
+
+        }
+
+        private static List<Person> DeserializeFromFile(string fileName)
+        {
+            var serializer = new XmlSerializer(typeof(Person));
+            var filestream = new FileStream(fileName, FileMode.Create);
+
+            List <Person> result = (List<Person>)serializer.Deserialize(filestream);
+            return result;
+
+        }
+
+
+
+
+
+        private static void Fillist(List<Person>list)
+        {
+            list.Add(new Person
+            {
+
+                ID = 1,
+                Name = new Name
+                {
+
+                    First = "Fred",
+                    Last = "Belotte"
+
+                },
+                Address = new Address
+                {
+                    Line1 = "",
+                    Line2 = "",
+                    City = "",
+                    State = "",
+                    ZipCode = "",
+
+                }
+            });
+
+        }
+            
+    }
+}
