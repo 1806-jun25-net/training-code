c960f29698bfd16515bf1f34da9fd8fcd3fef1d5
KyleTTownsend <KyleTTownsend@gmail,com>
2018-06-29 13:55:40 -0400

    updated branch

---
+++ Submissions/FIzzBuzz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {   
+        static void PrintFizzBuzz()
+        {
+            int fizz = 0, buzz = 0, fizzbuzz = 0;
+            for(int i = 1; i <= 1000; i++)
+            {
+                if(i % 3 == 0 && i % 5 ==0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                    fizzbuzz++;
+                }
+                else if(i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                    fizz++;
+                }
+                else if(i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                    buzz++;
+                }
+                else
+                    Console.WriteLine(i);
+            
+            }
+            Console.WriteLine("Fizz count: " + fizz);
+            Console.WriteLine("Buzz count: " + buzz);
+            Console.WriteLine("FizzBuzz count: " + fizzbuzz);
+        }
+        static void Main(string[] args)
+        {
+            PrintFizzBuzz();
+        }
+    }
+}
