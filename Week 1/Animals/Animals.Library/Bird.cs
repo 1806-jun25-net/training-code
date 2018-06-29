8a500e7ea98b77225d79bb5ac23855538806c364
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-06-29 09:51:04 -0400

    change animals example to demo constructor/override behavior.

--- Week 1/Animals/Animals.Library/Bird.cs
+++ Week 1/Animals/Animals.Library/Bird.cs
@@ -6,13 +6,28 @@ namespace Animals.Library
 {
     public class Bird : AAnimal
     {
+        public Bird(double wingSpan, string name = "Bob", int value = 4)
+        {
+            WingSpan = wingSpan;
+            Console.WriteLine($"Wing span is {wingSpan}");
+            Name = name;
+        }
+
+        //// one constructor can call another
+        //public Bird() : this(wingSpan: 2, value: 8)
+        //{
+
+        //}
+
+        public double WingSpan { get; set; }
+
         public override string Name { get; set; }
 
         public override string GoAction { get; set; } = "Flying";
 
         public override void MakeSound()
         {
-            Console.WriteLine("Cawww");
+            Console.WriteLine(value: "Cawww");
         }
     }
 }
