f85f1651bbc60e144e9181dec8b59db91f7cfa80
rolov3 <rolando.toledo827@gmail.com>
2018-06-29 09:53:48 -0400

    Changes to bird. Added BlueRobin. Constructors

--- Week 1/AnimalsUI/AnimalLibrary/Bird.cs
+++ Week 1/AnimalsUI/AnimalLibrary/Bird.cs
@@ -6,13 +6,28 @@ namespace Animals.Library
 {
     public class Bird : AAnimal
     {
+        public Bird(double wingSpan, string name = "Bob", int value = 3)
+        {
+            WingSpan = wingSpan;
+            Console.WriteLine($"Wing span is {wingSpan}");
+            Name = name;
+        }
+
+        //// one constructor can call another
+        //public Bird() : this(wingSpan: 2, value: 8)
+        //{
+
+        //}
+
+        public double WingSpan { get; set; }
+
         public override string Name { get; set; }
 
         public override object GoAction { get; set; } = "Flying";
 
         public override void MakeSound()
         {
-            Console.WriteLine("sing");
+            Console.WriteLine("Cawww");
         }
     }
 }
