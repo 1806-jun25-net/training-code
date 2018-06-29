bb02871422cd460fa43dc94e4fd45622e7428009
Cristian J. Rosario <cris0u0rosario@gmail.com>
2018-06-29 12:01:18 -0400

    update

--- Week 1/Animals/Animals.Library/Bird.cs
+++ Week 1/Animals/Animals.Library/Bird.cs
@@ -6,8 +6,18 @@ namespace Animals.Library
 {
     public class Bird : AAnimal
     {
-        public override string Name { get; set; }
+        public Bird(double wingSpan, String name = "Tweety") {
+            WingSpan = wingSpan;
+            Console.WriteLine($"wig span id {wingSpan}");
+            Name = name;
+        }
+        public Bird():this(2)//one constrctor can call another 
+        {
 
+        }
+        public double WingSpan { get; set; }
+        public override string Name { get; set; }
+      
         public override string GoAction { get; set; } = "Flying";
 
         public override void MakeSound()
---
+++ Week 1/Animals/Animals.Library/BlueRobin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals.Library
+{
+    class BlueRobin : Bird
+    {
+        public BlueRobin() : base (wingSpan: 3)
+        {
+
+        }
+    }
+}
