1b9fea09e29797b81d72566a63a1fc60d29fe962
ajzoell <ajzoellner@gmail.com>
2018-07-02 10:55:02 -0400

    created solution and classes to check if something is a palindrome. also created class that removes whitespace from a string

---
+++ Submissions/Palindrome/Plaindrome/Plaindrome.Library/Palindrome.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plaindrome.Library
+{
+    class Palindrome
+    {
+        public bool IsPalindrome(string palindrome)
+        {
+            bool SameBothWays = true;
+            string StringToCheck = "";
+
+            palindrome = palindrome.ToLower();
+            StringToCheck = RemoveWhiteSpace(palindrome);
+
+            for (int i = 0; i < StringToCheck.Length / 2; i++)
+            {
+                if (StringToCheck[i] != StringToCheck[StringToCheck.Length - i])
+                {
+                    SameBothWays = false;
+                }
+            }
+
+            return SameBothWays;
+        }
+
+        public string RemoveWhiteSpace(string pal)
+        {
+            string StringToCheck = "";
+            for (int i = 0; i < pal.Length; i++)
+            {
+                if (!Char.IsWhiteSpace(pal[i]))
+                {
+                    StringToCheck = StringToCheck + pal[i];
+                }
+
+            }
+            return StringToCheck;
+        }
+    }
+}
