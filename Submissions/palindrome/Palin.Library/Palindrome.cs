f2359a41dbfc64708a84ab306e34f6ed2f5e9df6
kevinhko <ko.kevin96@gmail.com>
2018-07-02 11:39:03 -0400

    added palindrome files

---
+++ Submissions/palindrome/Palin.Library/Palindrome.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Palin.Library
+{
+    public class Palindrome
+    {
+        public bool isPalindrome(string s)
+        {
+            //First remove all punctuations
+            string str = "";
+            foreach (char c in s)
+            {
+                if (!char.IsPunctuation(c))
+                {
+                    str += c;
+                }
+            }
+            //Then remove all whitespaces
+            str.Replace(" ", string.Empty);
+
+            //Finally, convert the string to lowercase
+            str.ToLower();
+
+
+            //Now rewrite the string backwards and check
+            string reverseStr = Reverse(str);
+
+            return str == reverseStr;
+        }
+
+        public string Reverse(string s)
+        {
+
+            string rev = "";
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                rev += s[i];
+            }
+            return rev;
+        }
+    }
+}
---
+++ Submissions/palindrome/Palin.Test/PalinTesting.cs
@@ -0,0 +1,47 @@
+﻿using Palin.Library;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Palin.Test
+{
+    public class PalinTesting
+    {
+        [Theory]
+        [InlineData(true, "nurses run")]
+        [InlineData(true, "racecaR")]
+        [InlineData(true, "1221")]
+        [InlineData(true, "never odd, or even.")]
+        public void basicTestsShouldReturnTrue(bool b, string s)
+        {
+            //Arrange
+            bool expected = true;
+            bool actual = b;
+
+            //Act
+            Palindrome p = new Palindrome();
+            actual = p.isPalindrome(s);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(false, "one, two, one")]
+        [InlineData(false, "123abccba123")]
+        public void basicTestsShouldReturnFalse(bool b, string s)
+        {
+            //Arrange
+            bool expected = true;
+            bool actual = b;
+
+            //Act
+            Palindrome p = new Palindrome();
+            actual = p.isPalindrome(s);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
