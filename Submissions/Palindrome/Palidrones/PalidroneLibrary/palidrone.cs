db07f234a9b57be60190e619aaaadd490d1b4872
Jean Carlo Semprit Rodriguez <jc.semprit@hotmail.com>
2018-07-02 11:41:35 -0400

    Submit challenge

---
+++ Submissions/Palindrome/Palidrones/PalidroneLibrary/palidrone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Palidrone.Library
+{
+    public static class Palidrone
+    {
+        public static Boolean IsPalidrone(String s) 
+        {
+            String original = s;
+            String isPalidrone = reverseString(original);
+            return (original == isPalidrone);
+        }
+
+        public static string reverseString(String s) {
+            return new string(s.Reverse().ToArray());
+        }
+    }
+}
---
+++ Submissions/Palindrome/Palidrones/PalidroneTest/UnitTest1.cs
@@ -0,0 +1,33 @@
+using System;
+using Xunit;
+using Palidrone.Library;
+
+namespace Palidrone.Test
+{
+    public class PalidroneTest
+    {
+        // TODO
+        // Test for correctly pass a palidrone world
+        // Test for correctly detect a none palidrone world
+        // Test detect when some one throw data type different to string
+        [Fact]
+        public void PalidroneShouldPassPalidroneWorld()
+        {
+
+        }
+
+
+        [Fact]
+        public void PalidroneShouldDetectNonePalidroneWorld()
+        {
+            
+        }
+
+        [Fact]
+        public void PalidroneShouldHandleJunkInput()
+        {
+            
+        }
+
+    }
+}
