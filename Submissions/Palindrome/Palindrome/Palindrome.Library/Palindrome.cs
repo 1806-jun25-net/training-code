cf408a280421d002d67cc46891b0189673c90cbe
rolov3 <rolando.toledo827@gmail.com>
2018-07-02 11:37:28 -0400

    Palindrome Coding Challenge

---
+++ Submissions/Palindrome/Palindrome/Palindrome.Library/Palindrome.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Palindrome.Library
+{
+    public class Palindrome
+    {
+        
+        public static bool IsPalindrome(string str)
+        {
+            str.ToLower();
+            str.Trim();
+                        
+            int count = 0;
+            for (int i = (str.Length / 2); i >= 0; i--)
+            {
+                if (str[count] == str[i]) { }
+                else { return false; }
+                count++;
+            }
+            return true;
+        }
+    }
+}
---
+++ Submissions/Palindrome/Palindrome/Palindrome.Test/PalindromeTest.cs
@@ -0,0 +1,22 @@
+﻿using Palindrome.Library;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Palindrome.Testing
+{
+    public class PalindromeTest
+    {
+        [Fact]
+        public void IsPalindromShouldReturnTrue()
+        {
+            bool expected = true;
+            string str = "nursesrun";
+
+            bool result = Palindrome.Library.Palindrome.IsPalindrome(str);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
