942e5954ba9de23383fc18c445b7ce13019bcd77
Brandon Kirk <bjkirk94@gmail.com>
2018-07-02 10:54:53 -0400

    Added Testing project and empty class to test with.

---
+++ Submissions/Palindrome/PalindromeChallenge/PalindromeChallenge.Testing/PalindromeTesterTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PalindromeChallenge.Testing
+{
+    public class PalindromeTesterTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
