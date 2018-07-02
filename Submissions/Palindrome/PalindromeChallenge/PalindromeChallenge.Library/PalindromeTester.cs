6c06d7e4d3060947e1a22dcd729da2bd22d29814
Brandon Kirk <bjkirk94@gmail.com>
2018-07-02 10:51:36 -0400

    Created PanlindromeChallenge solution.  Added library project and PalindromeTester class with functionality.

---
+++ Submissions/Palindrome/PalindromeChallenge/PalindromeChallenge.Library/PalindromeTester.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PalindromeChallenge.Library
+{
+    public static class PalindromeTester
+    {
+        public static bool IsPalindrome(string s)
+        {
+            //base case:
+            if (s.Length <= 1)
+                return true;
+            //recursive step: (can assume s.Length >=2
+            else
+            {
+                if (s[0] == s[s.Length - 1])
+                    return IsPalindrome(s.Substring(1, s.Length - 2));
+                else
+                    return false;
+            }
+        }
+    }
+}
