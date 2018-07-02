96027b7e6b8c15c3c98f43587bce65c4adf1cc5e
Eric Agocs <ago.er@yandex.com>
2018-07-02 13:33:31 -0400

    changed around directory

---
+++ Submissions/PizzaApp/PizzaLibrary/PizzaLibrary/PizzaClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PizzaLibrary
+{
+    public class PizzaClass
+    {
+        public bool stringTest(string s)
+        {
+            if (s is string) { return true; }
+            else return false;
+        }
+    }
+}
---
+++ Submissions/PizzaApp/PizzaLibrary/UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("48e0d03c-8abb-4e67-8003-805c16e4fe7b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
