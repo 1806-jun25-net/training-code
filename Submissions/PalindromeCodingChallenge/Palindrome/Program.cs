a1806b6d06dfa82bc1f70086736bc85fcabba1be
Daniel Ramskill <dramskil@uncc.edu>
2018-07-02 11:46:21 -0400

    commit

--- Submissions/PalindromeCodingChallenge/Palindrome/Program.cs
+++ Submissions/PalindromeCodingChallenge/Palindrome/Program.cs
@@ -8,7 +8,7 @@ namespace PalindromCodingChallenge
         public static void Main(string[] args)
         {
             var test = new Palindrome();
-            test.IsPalindrom();
+            Palindrome.IsPalindrom();
         }
     }
 }
