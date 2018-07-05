0969d7151505d077d6bc76e8ade926045a7dbe96
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-07-05 14:33:55 -0400

    make ef projects and scaffold dbcontext.

---
+++ Week 2/EFDBFirstDemo/EFDBFirstDemo.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EFDBFirstDemo.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ORM: object-relational mapper
+            // our ORM in .NET is: Entity Framework
+            // we will use database-first approach to EF
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Week 2/EFDBFirstDemo/EFDBFirstDemo.Data/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFDBFirstDemo.Data
+{
+    public partial class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int GenreId { get; set; }
+
+        public Genre Genre { get; set; }
+    }
+}
