66f8c7905d11294bf1f007ea86ab82ae8af82c65
kevinhko <ko.kevin96@gmail.com>
2018-07-06 11:51:30 -0400

    pushing what i got

---
+++ k week 2/EFDBFirstDemo/EFDBFirstDemo.Data/MovieRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFDBFirstDemo.Data
+{
+    class MovieRepository
+    {
+        private readonly MoviesDBContext _db;
+
+        public MovieRepository(MoviesDBContext db)
+        {
+            _db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+    }
+}
