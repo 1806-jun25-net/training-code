393027a3507b05ce0f92e9b4ad5e1adf47cd2011
Chris Coronado <cbcoronado@live.com>
2018-07-11 16:42:37 -0400

    7/11/18 training code

---
+++ Week 3/coronado/MVCAndCodeFirst/MVCAndCodeFirst/Controllers/PersonController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCAndCodeFirst.Controllers
+{
+    public class PersonController : Controller
+    {
+        [Route("Person/Index")]
+        public IActionResult Index()
+        {
+            [Route("Person/Index")]
+            return View();
+        }
+    }
+}
---
+++ Week 3/coronado/MVCAndCodeFirst/MVCAndCodeFirst/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAndCodeFirst.Models
+{
+    // this is going to be our context model
+    // but we will write it
+    // should be "POCO", "plain old CLR object"
+    // need parameter-less constructor (usually default)
+    // and public get-set properties
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+    }
+}
