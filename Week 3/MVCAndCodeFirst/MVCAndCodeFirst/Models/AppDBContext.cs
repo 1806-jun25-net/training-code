4c9130547cb519164b2d2af118e1602d7c7132c7
Wayne Knain <wknain@gra.midco.net>
2018-07-12 09:47:52 -0400

    in class demos

---
+++ Week 3/MVCAndCodeFirst/MVCAndCodeFirst/Models/AppDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAndCodeFirst.Models
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Person> Person { get; set; }
+    }
+}
---
+++ Week 3/MVCAndCodeFirst/MVCAndCodeFirst/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAndCodeFirst.Models
+{
+    //this is going to be our context model
+    //but we will write it
+    //needs to be a POCO -> plain old CLR object
+    //needs parameter-less constructor
+    //and public get-set properties
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+    }
+}
