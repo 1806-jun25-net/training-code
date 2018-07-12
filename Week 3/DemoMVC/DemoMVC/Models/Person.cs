976f5890e8749888acc10ea0326f38c78b115296
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-07-12 09:42:49 -0400

    add person crud to demomvc

---
+++ Week 3/DemoMVC/DemoMVC/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoMVC.Models
+{
+    // this is going to be our context model.
+    // but we will write it.
+    // should be a "POCO" "plain old CLR object"
+    // need parameter-less contructor (usually default)
+    // and public get-set properties
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+    }
+}
