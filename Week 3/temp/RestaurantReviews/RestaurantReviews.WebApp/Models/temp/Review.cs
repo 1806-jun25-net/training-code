789324bddacfbcd52ce383b1679bcb456031ed9d
Chris Coronado <cbcoronado@live.com>
2018-07-13 12:38:35 -0400

    merge from master

---
+++ Week 3/temp/RestaurantReviews/RestaurantReviews.WebApp/Models/temp/Review.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantReviews.WebApp.Models
+{
+    public class Review
+    {
+        public string ReviewerName { get; set; }
+        public int? Score { get; set; }
+        public string Text { get; set; }
+    }
+}
