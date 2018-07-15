d1ee4803765688b5b27d70067e6c74b8a3146aa1
SHADOW <angel.luis79@yahoo.com>
2018-07-15 00:26:13 -0400

    djdj

--- Week 3/RestaurantReviews/RestaurantReviews.WebApp/Models/Review.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RestaurantReviews.WebApp.Models
-{
-    public class Review
-    {
-        public string ReviewerName { get; set; }
-        public int? Score { get; set; }
-        public string Text { get; set; }
-    }
-}
