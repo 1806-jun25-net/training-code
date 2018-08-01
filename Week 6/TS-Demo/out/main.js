3e6e16ca6e8a54bce63db3ad663b2e977f9ab754
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-08-01 12:08:25 -0400

    add typescript example

--- Week 6/TS-Demo/out/main.js
+++ Week 6/TS-Demo/out/main.js
@@ -1,4 +1,12 @@
 "use strict";
+// anything exported has to be imported to be used.
+// you import from a string which means the relative path
+// to that file where it's defined.
+Object.defineProperty(exports, "__esModule", { value: true });
+// when writing good typescript, every class should be in its own file,
+// and exported/imported. this is like namespaces/using in c#.
+var ajax_1 = require("./ajax");
+var swcharacter_1 = require("./swcharacter");
 var Main = /** @class */ (function () {
     function Main() {
     }
@@ -6,7 +14,30 @@ var Main = /** @class */ (function () {
     // type annotations e.g. ": void"
     Main.Main = function () {
         console.log("Hello world");
-        return 5;
+        var ajax = new ajax_1.Ajax();
+        // querySelector, another way to getElementById or by class, etc.
+        // in typescript, we cast like this: "<newType> object"
+        // or, with "as":
+        var btn = document.querySelector("#searchBtn");
+        btn.addEventListener("click", function () {
+            var list = document.querySelector("#searchList");
+            var input = document.querySelector("#searchText");
+            var searchText = input.value;
+            var url = "https://swapi.co/api/people/?search=" + searchText;
+            ajax.send(url, 'get', function (text) {
+                var result = JSON.parse(text);
+                list.innerHTML = ""; // empty the list
+                for (var i = 0; i < result.results.length; i++) {
+                    var char = new swcharacter_1.SWCharacter(result.results[i].name);
+                    var newItem = document.createElement("li");
+                    newItem.innerHTML = char.description();
+                    list.appendChild(newItem);
+                }
+                ;
+            }, function (text) {
+                console.log("error: " + text);
+            });
+        });
     };
     return Main;
 }());
--- Week 6/TS-Demo/out/main.ts
+++ Week 6/TS-Demo/out/main.ts
@@ -1,3 +1,13 @@
+// anything exported has to be imported to be used.
+// you import from a string which means the relative path
+// to that file where it's defined.
+
+// when writing good typescript, every class should be in its own file,
+// and exported/imported. this is like namespaces/using in c#.
+
+import { Ajax } from "./ajax";
+import { SWCharacter } from "./swcharacter";
+
 class Main {
     constructor() {
 
@@ -7,6 +17,38 @@ class Main {
     // type annotations e.g. ": void"
     static Main() : void {
         console.log("Hello world");
+
+        let ajax = new Ajax();
+
+        // querySelector, another way to getElementById or by class, etc.
+        // in typescript, we cast like this: "<newType> object"
+        // or, with "as":
+
+        let btn = document.querySelector("#searchBtn") as HTMLButtonElement;
+        btn.addEventListener("click", () => {
+            let list = document.querySelector("#searchList") as HTMLOListElement;
+            let input = document.querySelector("#searchText") as HTMLInputElement;
+            let searchText = input.value;
+            let url = "https://swapi.co/api/people/?search=" + searchText;
+            ajax.send(
+                url,
+                'get',
+                text => {
+                    let result = JSON.parse(text);
+
+                    list.innerHTML = ""; // empty the list
+                    for (let i = 0; i < result.results.length; i++) {
+                        let char = new SWCharacter(result.results[i].name);
+                        let newItem = document.createElement("li");
+                        newItem.innerHTML = char.description();
+                        list.appendChild(newItem);
+                    };
+                },
+                text => {
+                    console.log("error: " + text);
+                });
+
+        });
     }
 }
 
