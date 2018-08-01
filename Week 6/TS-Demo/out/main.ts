2046fef572dc5a735ff0723e29a5d0344754f296
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-08-01 11:27:39 -0400

    start ts demo

---
+++ Week 6/TS-Demo/out/main.ts
@@ -0,0 +1,16 @@
+class Main {
+    constructor() {
+
+    }
+
+    // central addition of TypeScript:
+    // type annotations e.g. ": void"
+    static Main() : void {
+        console.log("Hello world");
+    }
+}
+
+// TypeScript also adds classes, interfaces, inheritance,
+// access modifiers
+
+Main.Main();
