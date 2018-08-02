4c93d78978ba7cc01a0289393cdf372aec5e771e
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-08-02 14:42:34 -0400

    add basic pokemon component

--- Week 6/pokemon-app/src/app/app.module.ts
+++ Week 6/pokemon-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonComponent
   ],
   imports: [
     BrowserModule
