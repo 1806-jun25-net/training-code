cb904f016405d83e6cba76f5dca6f33be412bdf1
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-08-02 15:15:37 -0400

    add api calling to pokemon example

--- Week 6/pokemon-app/src/app/pokemon/pokemon.component.ts
+++ Week 6/pokemon-app/src/app/pokemon/pokemon.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Pokemon } from '../models/pokemon';
+import { PokeapiService } from '../pokeapi.service';
 
 @Component({
   selector: 'app-pokemon',
@@ -11,9 +12,35 @@ export class PokemonComponent implements OnInit {
     // {name: "Bulbasaur", height: 20, weight: 20}
   ];
 
-  constructor() { }
+  searchText: string = "";
 
+  // component constructor is just for passing in parameters.
+  // the angular component structure is not really created yet
+  constructor(private api: PokeapiService) { }
+
+  // any setup to do with the view goes in ngOnInit
+  // ngOnInit is one of several "lifecycle hooks"
   ngOnInit() {
   }
 
+  getPokemon() {
+    if (this.searchText === "") {
+      this.api.getAllPokemon(
+        (res) => {
+          console.log("success");
+          console.log(JSON.stringify(res));
+          this.pokemons = res.results;
+        },
+        (res) => console.log("failure")
+      );
+    } else {
+      this.api.getPokemon(this.searchText,
+        (res) => {
+          // console.log(JSON.stringify(res));
+          this.pokemons = [res];
+        },
+        (res) => console.log("failure")
+      );
+    }
+  }
 }
